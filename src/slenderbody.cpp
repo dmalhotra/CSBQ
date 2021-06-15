2a09335166f64f82d78043a66f178ee27790d1f8
Dhairya Malhotra <contact@dhairyamalhotra.com>
2021-06-15 00:51:12 -0400

    update SCTL version and the examples

--- src/slenderbody.cpp
+++ src/slenderbody.cpp
@@ -2,52 +2,69 @@
 
 using namespace sctl;
 
-template <class Real> void double_layer_test() { // Double-layer identity test
-    Vector<SlenderElement<Real>> elem_lst0(8);
-    for (sctl::Long k = 0; k < elem_lst0.Dim(); k++) { // Init elem_lst0
-      const auto& nds = SlenderElement<Real>::CenterlineNodes();
-      Vector<Real> coord(nds.Dim()*3), radius(nds.Dim());
+template <class Real, class Kernel> void double_layer_test() { // Double-layer identity test
+  SlenderElemList<Real> elem_lst0;
+  elem_lst0.Read("data/geom.data"); // Read geometry from file
+  if (0) { // Initialize elem_lst0 in code
+    const Long Nelem = 16;
+    const Long ChebOrder = 10;
+    const Long FourierOrder = 14;
+
+    Vector<Real> coord, radius;
+    Vector<Long> cheb_order, fourier_order;
+    for (Long k = 0; k < Nelem; k++) {
+      cheb_order.PushBack(ChebOrder);
+      fourier_order.PushBack(FourierOrder);
+      const auto& nds = SlenderElemList<Real>::CenterlineNodes(ChebOrder);
       for (Long i = 0; i < nds.Dim(); i++) {
-        Real theta = 2*const_pi<Real>()*(k+nds[i])/(Real)elem_lst0.Dim();
-        coord[i*3+0] = cos<Real>(theta);
-        coord[i*3+1] = sin<Real>(theta);
-        coord[i*3+2] = 0.1*sin<Real>(2*theta);
-        radius[i] = 0.01*(2+sin<Real>(theta+sqrt<Real>(2)));
+        Real theta = 2*const_pi<Real>()*(k+nds[i])/Nelem;
+        coord.PushBack(cos<Real>(theta));
+        coord.PushBack(sin<Real>(theta));
+        coord.PushBack(0.1*sin<Real>(2*theta));
+        radius.PushBack(0.01*(2+sin<Real>(theta+sqrt<Real>(2))));
       }
-      elem_lst0[k].Init(coord,radius);
     }
-
-    GenericKernel<sctl::Laplace3D_DxU> laplace_dl;
-    BoundaryIntegralOp<Real,GenericKernel<sctl::Laplace3D_DxU>> LapDL(laplace_dl);
-    LapDL.AddElemList(elem_lst0);
-
-    // Warm-up run
-    Vector<Real> F(LapDL.Dim(0)), U; F = 1;
-    LapDL.ComputePotential(U,F);
-
-    sctl::Profile::Enable(true);
-    Profile::Tic("Setup+Eval");
-    LapDL.ClearSetup();
-    LapDL.ComputePotential(U,F);
-    Profile::Toc();
-
-    U = 0;
-    Profile::Tic("Eval");
-    LapDL.ComputePotential(U,F);
-    Profile::Toc();
-
-    Vector<Real> Uerr = U*(1/(4*const_pi<Real>())) - 0.5;
-    SlenderElement<Real>::WriteVTK("Uerr", elem_lst0, Uerr); // Write VTK
-    { // Print error
-      Real max_err = 0;
-      for (auto x : Uerr) max_err = std::max<Real>(max_err, fabs(x));
-      std::cout<<"Error = "<<max_err<<'\n';
-    }
-    sctl::Profile::print();
+    elem_lst0.Init(cheb_order, fourier_order, coord, radius);
   }
 
+  Kernel ker_fn;
+  BoundaryIntegralOp<Real,Kernel> BIOp(ker_fn);
+  BIOp.AddElemList(elem_lst0);
+  BIOp.SetAccuracy(1e-10);
+
+  // Warm-up run
+  Vector<Real> F(BIOp.Dim(0)), U; F = 1;
+  BIOp.ComputePotential(U,F);
+  BIOp.ClearSetup();
+
+  Profile::Enable(true);
+  Profile::Tic("Setup+Eval");
+  BIOp.ComputePotential(U,F);
+  Profile::Toc();
+
+  U = 0;
+  BIOp.ComputePotential(U,F);
+
+  Vector<Real> Uerr = U + 0.5;
+  elem_lst0.WriteVTK("Uerr_", Uerr); // Write VTK
+  { // Print error
+    Real max_err = 0;
+    for (auto x : Uerr) max_err = std::max<Real>(max_err, fabs(x));
+    std::cout<<"Error = "<<max_err<<'\n';
+  }
+  Profile::Enable(false);
+  Profile::print();
+}
+
 int main(int argc, char** argv) {
-  double_layer_test<double>();
+  std::cout<<"\n\n###########  DOUBLE-LAYER IDENTITY TEST WITH STOKES KERNEL  ############\n";
+  double_layer_test<double, GenericKernel<Stokes3D_DxU>>();
+
+  std::cout<<"\n\n###########  DOUBLE-LAYER IDENTITY TEST WITH LAPLACE KERNEL  ###########\n";
+  double_layer_test<double, GenericKernel<Laplace3D_DxU>>();
+
+  // SlenderElemList<double>::test_greens_identity();
+
   return 0;
 }
 
--- src/test-unitDLP-tangle.cpp
+++ src/test-unitDLP-tangle.cpp
@@ -11,7 +11,7 @@ template <class Real> void double_layer_test() { // Double-layer identity test
    std::default_random_engine g;
    g.seed(0);         // fix random seed
    std::normal_distribution<double> randn(0.0,1.0);
-  
+
    // build a tangle complicated curve as Fourier cos & sin series...
    int tangle_freq = 10;   // max Fourier freq, 20 in linequad paper w/ Ludvig
    Vector<Real> co(6*tangle_freq);       // choose coeffs
@@ -21,31 +21,45 @@ template <class Real> void double_layer_test() { // Double-layer identity test
      for (int i=0;i<6;++i)
        co[i + 6*(j-1)] = ampl*randn(g);
    }
-   int npan = 40;
-   Vector<SlenderElement<Real>> elem_lst0(npan);
-   for (sctl::Long k = 0; k < elem_lst0.Dim(); k++) {   // loop over panels...
-     const auto& nds = SlenderElement<Real>::CenterlineNodes();
-     Vector<Real> coord(nds.Dim()*3), radius(nds.Dim());   // just for this pan
-     coord = 0.0;              // init ctrline nodes for this pan
-     for (int j=1; j<=tangle_freq; ++j) {
-       // add in j'th Fourier mode w/ these coeffs, for this panel...
-       for (Long i = 0; i < nds.Dim(); i++) {     // loop over ctrline nodes
-         // param (theta) domain for the i'th node in panel...
-         Real theta = 2*const_pi<Real>()*(k+nds[i])/(Real)elem_lst0.Dim();
-         Real cmode = cos<Real>(j*theta), smode = sin<Real>(j*theta);
-         int o = 6*(j-1);     // offset index to j'th mode coeffs
-         coord[i*3+0] += cmode*co[0+o] + smode*co[3+o];
-         coord[i*3+1] += cmode*co[1+o] + smode*co[4+o];
-         coord[i*3+2] += cmode*co[2+o] + smode*co[5+o];
+   SlenderElemList<Real> elem_lst0;
+   { // Initialize elem_lst0
+     int npan = 40;
+     int elem_cheb_order = 10;
+     int elem_fourier_order = 8;
+
+     Vector<Real> coord, radius;
+     Vector<Long> cheb_order, fourier_order;
+     for (sctl::Long k = 0; k < npan; k++) {   // loop over panels...
+       cheb_order.PushBack(elem_cheb_order);
+       fourier_order.PushBack(elem_fourier_order);
+
+       const auto& nds = SlenderElemList<Real>::CenterlineNodes(elem_cheb_order);
+       Vector<Real> elem_coord(nds.Dim()*3), elem_radius(nds.Dim());   // just for this pan
+       elem_coord = 0.0;              // init ctrline nodes for this pan
+       for (int j=1; j<=tangle_freq; ++j) {
+         // add in j'th Fourier mode w/ these coeffs, for this panel...
+         for (Long i = 0; i < nds.Dim(); i++) {     // loop over ctrline nodes
+           // param (theta) domain for the i'th node in panel...
+           Real theta = 2*const_pi<Real>()*(k+nds[i])/npan;
+           Real cmode = cos<Real>(j*theta), smode = sin<Real>(j*theta);
+           int o = 6*(j-1);     // offset index to j'th mode coeffs
+           elem_coord[i*3+0] += cmode*co[0+o] + smode*co[3+o];
+           elem_coord[i*3+1] += cmode*co[1+o] + smode*co[4+o];
+           elem_coord[i*3+2] += cmode*co[2+o] + smode*co[5+o];
+         }
        }
+       for (Long i = 0; i < nds.Dim(); i++) {         // fill radii, this pan
+         Real theta = 2*const_pi<Real>()*(k+nds[i])/npan;
+         elem_radius[i] = 0.005 * (2+sin<Real>(theta+sqrt<Real>(2)));
+       }
+
+       // append to coord and radius
+       for (const auto x: elem_coord) coord.PushBack(x);
+       for (const auto r: elem_radius) radius.PushBack(r);
      }
-     for (Long i = 0; i < nds.Dim(); i++) {         // fill radii, this pan
-       Real theta = 2*const_pi<Real>()*(k+nds[i])/(Real)elem_lst0.Dim();
-       radius[i] = 0.005 * (2+sin<Real>(theta+sqrt<Real>(2)));
-     }
-     elem_lst0[k].Init(coord,radius);     // send coords into this pan
+     elem_lst0.Init(cheb_order, fourier_order, coord, radius);     // send coords into this pan
    }
-   
+
     GenericKernel<sctl::Laplace3D_DxU> laplace_dl;
     BoundaryIntegralOp<Real,GenericKernel<sctl::Laplace3D_DxU>> LapDL(laplace_dl);
     LapDL.AddElemList(elem_lst0);
@@ -65,8 +79,8 @@ template <class Real> void double_layer_test() { // Double-layer identity test
     LapDL.ComputePotential(U,F);
     Profile::Toc();
 
-    Vector<Real> Uerr = U*(1/(4*const_pi<Real>())) - 0.5;
-    SlenderElement<Real>::WriteVTK("tangleUerr", elem_lst0, Uerr); // Write VTK
+    Vector<Real> Uerr = U + 0.5;
+    elem_lst0.WriteVTK("tangleUerr", Uerr); // Write VTK
     { // Print error
       Real max_err = 0;
       for (auto x : Uerr) max_err = std::max<Real>(max_err, fabs(x));
